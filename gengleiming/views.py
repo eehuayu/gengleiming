964c13267a14cc2cbf011f684ccde17d1a22f8e7
eehuayu <244277947@qq.com>
2016-11-03 01:04:20 +0800

    print ip

--- gengleiming/views.py
+++ gengleiming/views.py
@@ -3,3 +3,13 @@ from django.views import generic
 
 class HomeView(generic.TemplateView):
     template_name = "gengleiming/home.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+        if 'HTTP_X_FORWARDED_FOR' in self.request.META:
+            client_ip = self.request.META['HTTP_X_FORWARDED_FOR']
+            client_ip = client_ip.split(",")[0]
+        else:
+            client_ip = self.request.META['REMOTE_ADDR']
+        print(client_ip)
+        return ctx
