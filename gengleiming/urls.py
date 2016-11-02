cd277aa024b398132b0925cae52f9f18e5203efa
raymon <244277947@qq.com>
2016-11-02 20:42:18 +0800

    login and show user

--- gengleiming/urls.py
+++ gengleiming/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomeView.as_view(), name="home"),
     url(r'^fortestapp/', include("fortestapp.urls", namespace='fortestapp')),
+    url(r'^account/', include("django.contrib.auth.urls", namespace='account')),
 ]
 
 
