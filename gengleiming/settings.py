d96b93b42b08299b53522e9fa01e3ccef8e1da78
eehuayu <244277947@qq.com>
2017-08-01 03:52:53 +0800

    allow hosts

--- gengleiming/settings.py
+++ gengleiming/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '*w2g1&jdag=y0%-0xdcv@kzai)t2=04)p=x3f#8^fb%po$mf!f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.gengleiming.com']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = '/'
 
