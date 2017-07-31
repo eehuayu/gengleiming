580459f6ba6767f279d9a6a8530430c618246c9a
eehuayu <244277947@qq.com>
2017-08-01 03:09:07 +0800

    static

--- gengleiming/settings.py
+++ gengleiming/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
+def location(x):
+    return os.path.join(BASE_DIR, x)
+
+# location = lambda x: os.path.join(BASE_DIR, x)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
@@ -123,3 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = location('static')
