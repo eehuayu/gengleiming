43ba85cb15518664c98fd420b875015cabbfc825
eehuayu <244277947@qq.com>
2017-08-04 02:27:01 +0800

    models

--- allapps/cms/models.py
+++ allapps/cms/models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from allapps.cms import config
 
 
 class Base(models.Model):
@@ -11,7 +13,7 @@ class Base(models.Model):
 
 
 class Category(Base):
-    name = models.CharField(verbose_name="名称", max_length=255, unique=True)
+    name = models.CharField(verbose_name="名称", max_length=100, unique=True)
     description = models.TextField(verbose_name="描述", null=True, blank=True)
 
     def __str__(self):
@@ -21,25 +23,14 @@ class Category(Base):
         verbose_name = verbose_name_plural = "类别"
 
 
-class KeyWord(Base):
-    word = models.CharField(verbose_name="名称", max_length=64, unique=True)
-
-    def __str__(self):
-        return self.word
-
-    class Meta:
-        verbose_name = verbose_name_plural = "关键字"
-
-
 class Article(Base):
-    from django.contrib.auth.models import User
-    user = models.ForeignKey(User, verbose_name="用户", related_name="contents")
-    category = models.ForeignKey(Category, verbose_name="类别", on_delete=models.PROTECT, related_name="contents")  # 删除外键之前要先删除级联，否则报错
-    keyword = models.ManyToManyField(KeyWord, verbose_name="关键字")
+    user = models.ForeignKey(User, verbose_name="用户", related_name="user")
     title = models.CharField(verbose_name="标题", max_length=255)
     content = models.TextField(verbose_name="内容")
-    visit_count = models.PositiveIntegerField(verbose_name="访问量", default=0)
-    like = models.PositiveIntegerField(verbose_name="喜欢数", default=0)
+    category = models.ForeignKey(Category, verbose_name="类别", on_delete=models.PROTECT, related_name="contents")   # 删除外键之前要先删除级联，否则报错
+    keywords = models.CharField(verbose_name="关键字", max_length=255)
+    visit_times = models.PositiveIntegerField(verbose_name="访问量", default=0)
+    like_times = models.PositiveIntegerField(verbose_name="喜欢数", default=0)
     update_time = models.DateTimeField(verbose_name="更新时间", auto_now=True)
 
     def __str__(self):
@@ -52,12 +43,13 @@ class Article(Base):
 
     @property
     def comment_count(self):
+        """
+        评论数
+        :return:
+        """
         article = Article.objects.get(id=self.id)
         article_comment_count = article.comments.count()
-        comment_comment_count = 0
-        for comment in article.comments.all():
-            comment_comment_count += Comment.objects.filter(reply=comment.id).count()
-        return article_comment_count + comment_comment_count
+        return article_comment_count
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
@@ -66,13 +58,11 @@ class Article(Base):
 
 class Comment(Base):
     article = models.ForeignKey(Article, verbose_name="文章", null=True, blank=True, related_name="comments")
-    reply = models.PositiveIntegerField(verbose_name="回复评论id", help_text="这里是回复的评论的ID", default=None)
     content = models.TextField(verbose_name="内容")
-    like = models.PositiveIntegerField(verbose_name="点赞数", default=0)
-    is_article = models.BooleanField(verbose_name="是否文章留言", help_text="是文章下面的留言，或者是留言板的留言")
+    type = models.PositiveSmallIntegerField(verbose_name="留言的类型", choices=config.COMMENT_TYPE)
 
     def __str__(self):
-        return "文章评论:" + self.article.title if self.article else "回复评论:" + str(self.reply)
+        return dict(config.COMMENT_TYPE)[self.type] + ":" + self.article.title
 
     class Meta:
         verbose_name = verbose_name_plural = "留言"
--- allapps/cms/urls.py
+++ allapps/cms/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import article
 
 urlpatterns = [
-    url(r"^$", article.ArticleListView.as_view(), name="index"),
+    # url(r"^$", article.ArticleListView.as_view(), name="index"),
     url(r"write/$", article.ArticleCreateView.as_view(), name="write"),
-    url(r"category/create/$", article.CategoryCreateView.as_view(), name="category-create"),
-    url(r"read/(?P<pk>[\d]+)/$", article.ReadDetailView.as_view(), name="read"),
+    # url(r"category/create/$", article.CategoryCreateView.as_view(), name="category-create"),
+    # url(r"read/(?P<pk>[\d]+)/$", article.ReadDetailView.as_view(), name="read"),
 ]
