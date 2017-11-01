91f541f447219a172f699259938ba3a7695a8c69
raymon <244277947@qq.com>
2017-11-01 23:47:42 +0800

    unicode test

--- gengleiming/views.py
+++ gengleiming/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from allapps.cms import models
 from allapps.cms import mixins
@@ -17,16 +18,14 @@ class FailView(mixins.TemplateViewWithCategory):
     template_name = "gengleiming/fail.html"
 
 
+@csrf_exempt
 def unicode_test(request):
-    if request.method != "post":
-        return HttpResponse("method is not post.")
+    if request.method != "GET":
+        return HttpResponse("method is not post, method is " + request.method)
 
-    data = request.POST.get("data")
+    data = request.GET.get("data")
 
     if not data:
         return HttpResponse("data is None.")
-
-    print(type(data))
-    print(data)
 
     return HttpResponse(data)
