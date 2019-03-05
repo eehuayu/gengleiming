643758d8e34d7f2a34adb020c67c11f2103fbb4a
kormay <244277947@qq.com>
2019-03-05 21:36:04 +0800

    root

--- gengleiming/settings.py
+++ gengleiming/settings.py
@@ -132,7 +132,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # --------------------- 日志系统的配置 --------------------------
 
