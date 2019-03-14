4ac37972e85d588ad829f7f1106521473bfa0a2d
kormay <244277947@qq.com>
2019-03-14 15:13:56 +0800

    honcho

--- gengleiming/settings.py
+++ gengleiming/settings.py
@@ -82,8 +82,8 @@ if os.environ.get("ONLINE") == "True":
         'default': {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'gengleiming',
-            'USER': 'gengleiming',
-            'PASSWORD': '521125',
+            'USER': 'root',
+            'PASSWORD': '123456',
             'HOST': '127.0.0.1',
             'PORT': '3306',
         }
