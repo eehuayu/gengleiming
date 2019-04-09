1de509bd56eb571b3fba032a8387dd8c0db1daaf
kormay <244277947@qq.com>
2019-04-09 16:54:28 +0800

    sqlite

--- gengleiming/settings.py
+++ gengleiming/settings.py
@@ -77,24 +77,24 @@ WSGI_APPLICATION = 'gengleiming.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-if os.environ.get("ONLINE") == "True":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'gengleiming',
-            'USER': 'root',
-            'PASSWORD': '123456',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-        }
+# if os.environ.get("ONLINE") == "True":
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': 'gengleiming',
+#             'USER': 'root',
+#             'PASSWORD': '123456',
+#             'HOST': '127.0.0.1',
+#             'PORT': '3306',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+}
 
 
 # Password validation
